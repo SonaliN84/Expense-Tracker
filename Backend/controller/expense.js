ac4c459148dcd521511d1f1adb41ec4aab4b7fb7
SonaliN84 <sonali.nilekar@gmail.com>
2023-04-10 19:52:01 +0530

    Using sequelize transactions

--- Backend/controller/expense.js
+++ Backend/controller/expense.js
@@ -1,4 +1,5 @@
 const Expense=require('../models/expense')
+const sequelize=require('../util/database')
 
 function isStringInValid(string){
     if(string===undefined ||string===null ||string.length===0){
@@ -12,6 +13,7 @@ function isStringInValid(string){
 
 exports.postAddExpense=async(req,res,next)=>{
     try{
+    const t=await sequelize.transaction()
     const amount=req.body.amount;
     const description=req.body.description;
     const category=req.body.category;
@@ -25,14 +27,18 @@ exports.postAddExpense=async(req,res,next)=>{
        amount:amount,
        description:description,
        category:category
-    })
-    req.user.update({
+    },{transaction:t})
+    await req.user.update({
         totalexpense:req.user.totalexpense+Number.parseInt(amount)
-    })
+    },{transaction:t})
+
+    await t.commit();
+
     return res.status(201).json(expense)
   }
   catch(err){
         console.log(err)
+       await t.rollback(); 
        return res.status(500).json({error:err,success:false})
     }  
 }
@@ -49,6 +55,7 @@ exports.getExpenses=async(req,res,next)=>{
 
 
 exports.deleteExpense=async(req,res,next)=>{
+    const t=await sequelize.transaction();
     try{
     const expenseId=req.params.expenseId;
 
@@ -57,13 +64,19 @@ exports.deleteExpense=async(req,res,next)=>{
         return res.status(400).json({success:false,message:"Bad parameters"})
     }
    
-    const expenses=await req.user.getExpenses({where:{id:expenseId}})
+    const expenses=await req.user.getExpenses({where:{id:expenseId}},{transaction:t})
       const expense=expenses[0]
       expense.destroy();
+    
+      await req.user.update({
+        totalexpense:req.user.totalexpense-expense.amount
+      },{transaction:t})
      
+       await t.commit();
      return  res.status(200).json({success:true,message:"Deleted successfully"})
     }
     catch(err){
+        await t.rollback();
         return res.status(500).json({error:err,success:false}) 
         }
 }
